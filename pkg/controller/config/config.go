006627353358ede2529e815a7344dfe1796cc78d
Rui Cao <rcao@vmware.com>
2020-09-01 18:42:53 -0700

    Implement ConfigController
    1.Add antreainstall CRD which is used to holds configurations for
      antrea deployment.
    2.Impelement main logic of ConfigController. ConfigController watch
      network cluster configurations and antreainstall CR changes. And
      update the antrea deployment if any configurations change.
    
    Signed-off-by: Rui Cao <rcao@vmware.com>

---
+++ pkg/controller/config/config.go
@@ -0,0 +1,220 @@
+/* Copyright © 2020 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0 */
+
+package config
+
+import (
+	"fmt"
+
+	configv1 "github.com/openshift/api/config/v1"
+	"github.com/openshift/cluster-network-operator/pkg/render"
+	"gopkg.in/yaml.v2"
+
+	operatorv1 "github.com/ruicao93/antrea-operator/pkg/apis/operator/v1"
+	"github.com/ruicao93/antrea-operator/pkg/types"
+)
+
+func FillConfigs(clusterConfig *configv1.Network, operConfig *operatorv1.AntreaInstall) error {
+	antreaAgentConfig := make(map[string]interface{})
+	err := yaml.Unmarshal([]byte(operConfig.Spec.AntreaAgentConfig), &antreaAgentConfig)
+	if err != nil {
+		return fmt.Errorf("failed to parse AntreaAgentConfig: %v", err)
+	}
+
+	// Set service CIDR.
+	if len(clusterConfig.Spec.ServiceNetwork) == 0 {
+		return fmt.Errorf("service network can not be empty")
+	}
+	serviceCIDR, ok := antreaAgentConfig[types.ServiceCIDROption]
+	if ok {
+		found := false
+		for _, serviceNet := range clusterConfig.Spec.ServiceNetwork {
+			if serviceNet == serviceCIDR {
+				found = true
+				break
+			}
+		}
+		if !found {
+			log.Info("WARNING: option: %s is overwritten by cluster config")
+			antreaAgentConfig[types.ServiceCIDROption] = clusterConfig.Spec.ServiceNetwork[0]
+		}
+	} else {
+		antreaAgentConfig[types.ServiceCIDROption] = clusterConfig.Spec.ServiceNetwork[0]
+	}
+
+	// Set default MTU.
+	_, ok = antreaAgentConfig[types.DefaultMTUOption]
+	if !ok {
+		antreaAgentConfig[types.DefaultMTUOption] = types.DefaultMTU
+	}
+
+	// Set Antrea image.
+	if operConfig.Spec.AntreaImage == "" {
+		operConfig.Spec.AntreaImage = types.DefaultAntreaImage
+	}
+
+	updatedAntreaAgentConfig, err := yaml.Marshal(antreaAgentConfig)
+	if err != nil {
+		return fmt.Errorf("failed to fill configurations in AntreaAgentConfig: %v", err)
+	}
+	operConfig.Spec.AntreaAgentConfig = string(updatedAntreaAgentConfig)
+	return nil
+}
+
+func ValidateConfig(clusterConfig *configv1.Network, operConfig *operatorv1.AntreaInstall) error {
+	var errs []error
+
+	// Validate antrea config
+	antreaAgentConfig := make(map[string]interface{})
+	err := yaml.Unmarshal([]byte(operConfig.Spec.AntreaAgentConfig), &antreaAgentConfig)
+	if err != nil {
+		errs = append(errs, fmt.Errorf("failed to parse AntreaAgentConfig: %v", err))
+	} else {
+		serviceCIDR, ok := antreaAgentConfig[types.ServiceCIDROption].(string)
+		if ok {
+			found := false
+			for _, serviceNet := range clusterConfig.Spec.ServiceNetwork {
+				if serviceNet == serviceCIDR {
+					found = true
+					break
+				}
+			}
+			if !found {
+				errs = append(errs, fmt.Errorf("invalid serviceCIDR option: %s, available values are: %s", serviceCIDR, clusterConfig.Spec.ServiceNetwork))
+			}
+		} else {
+			errs = append(errs, fmt.Errorf("serviceCIDR option can not be empty"))
+		}
+	}
+	if operConfig.Spec.AntreaImage == "" {
+		errs = append(errs, fmt.Errorf("antreaImage option can not be empty"))
+	}
+	if len(errs) > 0 {
+		return fmt.Errorf("invalidate configuration: %v", errs)
+	}
+	return nil
+}
+
+func NeedApplyChange(preConfig, curConfig *operatorv1.AntreaInstall) (agentNeedChange, controllerNeedChange, imageChange bool) {
+	if preConfig == nil {
+		return true, true, false
+	}
+
+	if preConfig.Spec.AntreaAgentConfig != curConfig.Spec.AntreaAgentConfig {
+		agentNeedChange = true
+	}
+	if preConfig.Spec.AntreaCNIConfig != curConfig.Spec.AntreaCNIConfig {
+		agentNeedChange = true
+	}
+	if preConfig.Spec.AntreaControllerConfig != curConfig.Spec.AntreaControllerConfig {
+		controllerNeedChange = true
+	}
+	if preConfig.Spec.AntreaImage != curConfig.Spec.AntreaImage {
+		agentNeedChange = true
+		controllerNeedChange = true
+		imageChange = true
+	}
+	return
+}
+
+func HasClusterNetworkConfigChange(preConfig, curConfig *configv1.Network) bool {
+	// TODO: We may need to save the applied cluster network config in somewhere else. Thus operator can
+	// retrieve the applied config on restart.
+	if preConfig == nil {
+		return true
+	}
+	if !stringSliceEqual(preConfig.Spec.ServiceNetwork, curConfig.Spec.ServiceNetwork) {
+		return true
+	}
+	var preCIDRs, curCIDRs []string
+	for _, clusterNet := range preConfig.Spec.ClusterNetwork {
+		preCIDRs = append(preCIDRs, clusterNet.CIDR)
+	}
+	for _, clusterNet := range curConfig.Spec.ClusterNetwork {
+		curCIDRs = append(curCIDRs, clusterNet.CIDR)
+	}
+	if !stringSliceEqual(preCIDRs, curCIDRs) {
+		return true
+	}
+	return false
+}
+
+func HasDefaultMTUChange(preConfig, curConfig *operatorv1.AntreaInstall) (bool, int, error) {
+
+	curAntreaAgentConfig := make(map[string]interface{})
+	err := yaml.Unmarshal([]byte(curConfig.Spec.AntreaAgentConfig), &curAntreaAgentConfig)
+	if err != nil {
+		return false, types.DefaultMTU, err
+	}
+	curDefaultMTU, ok := curAntreaAgentConfig[types.DefaultMTUOption]
+	if !ok {
+		return false, types.DefaultMTU, fmt.Errorf("%s option can not be empty", types.DefaultMTUOption)
+	}
+
+	if preConfig == nil {
+		return true, curDefaultMTU.(int), nil
+	}
+
+	preAntreaAgentConfig := make(map[string]interface{})
+	err = yaml.Unmarshal([]byte(preConfig.Spec.AntreaAgentConfig), &preAntreaAgentConfig)
+	if err != nil {
+		return false, types.DefaultMTU, err
+	}
+	preDefaultMTU, ok := preAntreaAgentConfig[types.DefaultMTUOption]
+	if !ok {
+		return false, types.DefaultMTU, fmt.Errorf("%s option can not be empty", types.DefaultMTUOption)
+	}
+
+	return preDefaultMTU != curDefaultMTU, curDefaultMTU.(int), nil
+}
+
+func BuildNetworkStatus(clusterConfig *configv1.Network, defaultMTU int) *configv1.NetworkStatus {
+	// Values extracted from spec are serviceNetwork and clusterNetworkCIDR.
+	status := configv1.NetworkStatus{}
+	for _, snet := range clusterConfig.Spec.ServiceNetwork {
+		status.ServiceNetwork = append(status.ServiceNetwork, snet)
+	}
+
+	for _, cnet := range clusterConfig.Spec.ClusterNetwork {
+		status.ClusterNetwork = append(status.ClusterNetwork,
+			configv1.ClusterNetworkEntry{
+				CIDR:       cnet.CIDR,
+				HostPrefix: cnet.HostPrefix,
+			})
+	}
+	status.NetworkType = clusterConfig.Spec.NetworkType
+	status.ClusterNetworkMTU = defaultMTU
+	return &status
+}
+
+func inSlice(str string, s []string) bool {
+	for _, v := range s {
+		if str == v {
+			return true
+		}
+	}
+	return false
+}
+
+func stringSliceEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for _, v := range a {
+		if !inSlice(v, b) {
+			return false
+		}
+	}
+	return true
+}
+
+func GenerateRenderData(operConfig *operatorv1.AntreaInstall) (*render.RenderData, error) {
+	renderData := render.MakeRenderData()
+
+	renderData.Data[types.AntreaAgentConfigRenderKey] = operConfig.Spec.AntreaAgentConfig
+	renderData.Data[types.AntreaCNIConfigRenderKey] = operConfig.Spec.AntreaCNIConfig
+	renderData.Data[types.AntreaControllerConfigRenderKey] = operConfig.Spec.AntreaControllerConfig
+	renderData.Data[types.AntreaImageRenderKey] = operConfig.Spec.AntreaImage
+
+	return &renderData, nil
+}
---
+++ pkg/controller/config/config_test.go
@@ -0,0 +1,219 @@
+/* Copyright © 2020 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0 */
+
+package config
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ghodss/yaml"
+	. "github.com/onsi/gomega"
+	configv1 "github.com/openshift/api/config/v1"
+	"github.com/openshift/cluster-network-operator/pkg/render"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	operatorv1 "github.com/ruicao93/antrea-operator/pkg/apis/operator/v1"
+	operatortypes "github.com/ruicao93/antrea-operator/pkg/types"
+)
+
+var mockClusterConfig = configv1.Network{
+	Spec: configv1.NetworkSpec{
+		ServiceNetwork: []string{"10.96.0.0/12"},
+		ClusterNetwork: []configv1.ClusterNetworkEntry{
+			{
+				CIDR:       "192.168.0.0/16",
+				HostPrefix: 24,
+			},
+		},
+		NetworkType: "antrea",
+	},
+}
+
+var mockOperConfig = operatorv1.AntreaInstall{
+	Spec: operatorv1.AntreaInstallSpec{
+		AntreaAgentConfig: "",
+		AntreaCNIConfig: `{
+    "cniVersion":"0.3.0",
+    "name": "antrea",
+    "plugins": [
+        {
+            "type": "antrea",
+            "ipam": {
+                "type": "host-local"
+            }
+        },
+        {
+            "type": "portmap",
+            "capabilities": {"portMappings": true}
+        }
+    ]
+}`,
+		AntreaControllerConfig: "apiPort: 10349",
+	},
+}
+
+func TestFillDefaults(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	clusterConfig := mockClusterConfig.DeepCopy()
+	operConfig := mockOperConfig.DeepCopy()
+	err := FillConfigs(clusterConfig, operConfig)
+	g.Expect(err).ShouldNot(HaveOccurred())
+
+	antreaAgentConfig := make(map[string]interface{})
+	err = yaml.Unmarshal([]byte(operConfig.Spec.AntreaAgentConfig), &antreaAgentConfig)
+	g.Expect(err).ShouldNot(HaveOccurred())
+	g.Expect(antreaAgentConfig[operatortypes.ServiceCIDROption]).Should(Equal(clusterConfig.Spec.ServiceNetwork[0]))
+	g.Expect(int(antreaAgentConfig[operatortypes.DefaultMTUOption].(float64))).Should(Equal(operatortypes.DefaultMTU))
+	g.Expect(operConfig.Spec.AntreaImage).Should(Equal(operatortypes.DefaultAntreaImage))
+}
+
+func TestValidateConfig(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	clusterConfig := mockClusterConfig.DeepCopy()
+	operConfig := mockOperConfig.DeepCopy()
+	err := FillConfigs(clusterConfig, operConfig)
+	g.Expect(err).ShouldNot(HaveOccurred())
+	err = ValidateConfig(clusterConfig, operConfig)
+	g.Expect(err).ShouldNot(HaveOccurred())
+
+	// Validate service CIDR
+	clusterConfig = mockClusterConfig.DeepCopy()
+	operConfig = mockOperConfig.DeepCopy()
+	clusterConfig.Spec.ServiceNetwork = []string{"10.96.0.0.0/12"}
+	operConfig.Spec.AntreaAgentConfig = "serviceCIDR: 10.96.0.0.1/12"
+	err = ValidateConfig(clusterConfig, operConfig)
+	g.Expect(err).Should(HaveOccurred())
+	g.Expect(err.Error()).Should(ContainSubstring("invalid serviceCIDR option"))
+	g.Expect(err.Error()).Should(ContainSubstring("available values are"))
+
+	// Validate antrea-agent config
+	clusterConfig = mockClusterConfig.DeepCopy()
+	operConfig = mockOperConfig.DeepCopy()
+	operConfig.Spec.AntreaAgentConfig = `serviceCIDR:---`
+	err = ValidateConfig(clusterConfig, operConfig)
+	g.Expect(err).Should(HaveOccurred())
+	g.Expect(err.Error()).Should(ContainSubstring("failed to parse AntreaAgentConfig"))
+}
+
+func TestRender(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	clusterConfig := mockClusterConfig.DeepCopy()
+	operConfig := mockOperConfig.DeepCopy()
+	err := FillConfigs(clusterConfig, operConfig)
+	g.Expect(err).ShouldNot(HaveOccurred())
+	renderData, err := GenerateRenderData(operConfig)
+	g.Expect(err).ShouldNot(HaveOccurred())
+	objs, err := render.RenderDir("../../../manifest", renderData)
+	g.Expect(err).ShouldNot(HaveOccurred())
+
+	for _, obj := range objs {
+		if obj.GetKind() == "ConfigMap" && obj.GetNamespace() == "kube-system" && obj.GetName() == "antrea-config" {
+			var data map[string]interface{}
+			data = obj.Object["data"].(map[string]interface{})
+			g.Expect(data[operatortypes.AntreaAgentConfigOption]).Should(Equal(operConfig.Spec.AntreaAgentConfig))
+			g.Expect(data[operatortypes.AntreaCNIConfigOption]).Should(Equal(operConfig.Spec.AntreaCNIConfig))
+			g.Expect(data[operatortypes.AntreaControllerConfigOption]).Should(Equal(operConfig.Spec.AntreaControllerConfig))
+		} else if obj.GetKind() == "Deployment" && obj.GetNamespace() == "kube-system" && obj.GetName() == "antrea-controller" {
+			antreaDeployment := &appsv1.Deployment{}
+			err = runtime.DefaultUnstructuredConverter.FromUnstructured(obj.UnstructuredContent(), antreaDeployment)
+			g.Expect(err).ShouldNot(HaveOccurred())
+			g.Expect(antreaDeployment.Spec.Template.Spec.Containers[0].Image).Should(Equal(operatortypes.DefaultAntreaImage))
+		}
+	}
+}
+
+func TestHasClusterNetworkConfigChange(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	preConfig := mockClusterConfig.DeepCopy()
+	curConfig := mockClusterConfig.DeepCopy()
+	changed := HasClusterNetworkConfigChange(preConfig, curConfig)
+	g.Expect(changed).Should(Equal(false))
+
+	// Service network change.
+	curConfig.Spec.ServiceNetwork = []string{"10.97.0.0/12"}
+	changed = HasClusterNetworkConfigChange(preConfig, curConfig)
+	g.Expect(changed).Should(Equal(true))
+
+	curConfig = mockClusterConfig.DeepCopy()
+	curConfig.Spec.ServiceNetwork = append(curConfig.Spec.ServiceNetwork, "10.97.0.0/12")
+	changed = HasClusterNetworkConfigChange(preConfig, curConfig)
+	g.Expect(changed).Should(Equal(true))
+
+	// Cluster network change.
+	curConfig = mockClusterConfig.DeepCopy()
+	curConfig.Spec.ClusterNetwork = []configv1.ClusterNetworkEntry{
+		{
+			CIDR:       "192.169.0.0/16",
+			HostPrefix: 24,
+		},
+	}
+	changed = HasClusterNetworkConfigChange(preConfig, curConfig)
+	g.Expect(changed).Should(Equal(true))
+
+	curConfig = mockClusterConfig.DeepCopy()
+	curConfig.Spec.ClusterNetwork = append(curConfig.Spec.ClusterNetwork, configv1.ClusterNetworkEntry{
+		CIDR:       "192.169.0.0/16",
+		HostPrefix: 24,
+	})
+	changed = HasClusterNetworkConfigChange(preConfig, curConfig)
+	g.Expect(changed).Should(Equal(true))
+}
+
+func TestHasDefaultMTUChange(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	testPreMtu := 1500
+	preConfig := mockOperConfig.DeepCopy()
+	preConfig.Spec.AntreaAgentConfig = fmt.Sprintf("%s: %d", operatortypes.DefaultMTUOption, testPreMtu)
+	curConfig := preConfig.DeepCopy()
+	defaultMTUChanged, curDefaultMTU, err := HasDefaultMTUChange(preConfig, curConfig)
+	g.Expect(err).ShouldNot(HaveOccurred())
+	g.Expect(defaultMTUChanged).Should(Equal(false))
+	g.Expect(curDefaultMTU).Should(Equal(testPreMtu))
+
+	testCurMtu := 1600
+	curConfig.Spec.AntreaAgentConfig = fmt.Sprintf("%s: %d", operatortypes.DefaultMTUOption, testCurMtu)
+	defaultMTUChanged, curDefaultMTU, err = HasDefaultMTUChange(preConfig, curConfig)
+	g.Expect(err).ShouldNot(HaveOccurred())
+	g.Expect(defaultMTUChanged).Should(Equal(true))
+	g.Expect(curDefaultMTU).Should(Equal(testCurMtu))
+
+	preConfig = mockOperConfig.DeepCopy()
+	curConfig = mockOperConfig.DeepCopy()
+	_, _, err = HasDefaultMTUChange(preConfig, curConfig)
+	g.Expect(err).Should(HaveOccurred())
+}
+
+func TestBuildNetworkStatus(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	defaultMtu := 1500
+	clusterConfig := mockClusterConfig.DeepCopy()
+	updatedClusterConfig := BuildNetworkStatus(clusterConfig, defaultMtu)
+
+	comparedClusterConfig := mockClusterConfig.DeepCopy()
+	comparedClusterConfig.Spec.ServiceNetwork = updatedClusterConfig.ServiceNetwork
+	comparedClusterConfig.Spec.ClusterNetwork = updatedClusterConfig.ClusterNetwork
+	g.Expect(HasClusterNetworkConfigChange(clusterConfig, comparedClusterConfig)).Should(Equal(false))
+	g.Expect(updatedClusterConfig.NetworkType).Should(Equal(clusterConfig.Spec.NetworkType))
+	g.Expect(updatedClusterConfig.ClusterNetworkMTU).Should(Equal(defaultMtu))
+}
+
+func TestGenerateRenderData(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	operConfig := mockOperConfig.DeepCopy()
+	operConfig.Spec.AntreaImage = operatortypes.DefaultAntreaImage
+	renderData, err := GenerateRenderData(operConfig)
+	g.Expect(err).ShouldNot(HaveOccurred())
+	g.Expect(renderData.Data[operatortypes.AntreaAgentConfigRenderKey]).Should(Equal(operConfig.Spec.AntreaAgentConfig))
+	g.Expect(renderData.Data[operatortypes.AntreaCNIConfigRenderKey]).Should(Equal(operConfig.Spec.AntreaCNIConfig))
+	g.Expect(renderData.Data[operatortypes.AntreaControllerConfigRenderKey]).Should(Equal(operConfig.Spec.AntreaControllerConfig))
+	g.Expect(renderData.Data[operatortypes.AntreaImageRenderKey]).Should(Equal(operConfig.Spec.AntreaImage))
+}
