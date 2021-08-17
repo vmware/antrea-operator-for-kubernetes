56acfa9ed5d6fefda08494cb347b6e61d9abbfcc
jsui <jsui@vmware.com>
2021-08-17 10:29:10 +0800

    update Dockerfile for specifying OVS and CNI version
    The version of OVS and CNI can be specified by user for building
    Antrea UBI image.

--- build/antrea/build_ubi.sh
+++ build/antrea/build_ubi.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-_usage="Usage: $0 [--tag <ImageTag>] [--antrea-version <AntreaVersion>] 
+_usage="Usage: $0 [--tag <ImageTag>] [--antrea-version <AntreaVersion>] [--ovs-version <Version>] 
+                  [--cni-version <Version> ]
 Generate Antrea UBI image.
-       --tag                                                   Specify the ip of vc.
-       --antrea-version                                        Specify the password of vc.
+       --tag                                                   Specify the image tag.
+       --antrea-version                                        Specify Antrea version.
+       --ovs-version                                           Specify the OVS version.
+       --cni-version                                           Specify the CNI version.
        --help, -h                                              Print usage message.
 "
 
@@ -23,6 +26,14 @@ case $key in
     ;;
     --antrea-version)
     ANTREA_VERSION="$2"
+    shift 2
+    ;;
+    --ovs-version)
+    OVS_VERSION="$2"
+    shift 2
+    ;;
+    --cni-version)
+    CNI_VERSION="$2"
     shift 2
     ;;
     -h|--help)
@@ -46,9 +57,19 @@ if [ -z "${ANTREA_VERSION}" ]; then
   echo "Using default Antrea version ${ANTREA_VERSION}."
 fi
 
+if [ -z "${OVS_VERSION}" ]; then
+  OVS_VERSION="2.14.2"
+  echo "Using default OVS version ${OVS_VERSION}."
+fi
+
+if [ -z "${CNI_VERSION}" ]; then
+  CNI_VERSION="V0.8.7"
+  echo "Using default CNI version ${CNI_VERSION}."
+fi
+
 wget https://github.com/antrea-io/antrea/archive/refs/tags/v${ANTREA_VERSION}.tar.gz && 
 tar -xzvf v${ANTREA_VERSION}.tar.gz
 
-docker build -t antrea/ovs-ubi:2.14.0 -f ./ovs/Dockerfile.ubi ./ovs
-docker build -t antrea/base-ubi:2.14.0 -f ./base/Dockerfile.ubi ./base
-docker build -t antrea/antrea-ubi:${IMAGE_TAG} -f ./Dockerfile.ubi ./antrea-${ANTREA_VERSION}
+docker build -t antrea/ovs-ubi:${OVS_VERSION} --build-arg OVS_VERSION=${OVS_VERSION} -f ./ovs/Dockerfile.ubi ./ovs
+docker build -t antrea/base-ubi:${OVS_VERSION} --build-arg OVS_VERSION=${OVS_VERSION} --build-arg CNI_BINARIES_VERSION=${CNI_VERSION} -f ./base/Dockerfile.ubi ./base
+docker build -t antrea/antrea-ubi:${IMAGE_TAG} --build-arg OVS_VERSION=${OVS_VERSION} -f ./Dockerfile.ubi ./antrea-${ANTREA_VERSION}
